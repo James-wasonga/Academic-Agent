455bb5a018f5573db3c55347f9949b5ea70a5483
James-wasonga <jameswasonga22@gmail.com>
2025-10-09 23:53:03 +0300

    remove comment codes

--- Frontend/src/components/Research/Research.jsx
+++ Frontend/src/components/Research/Research.jsx
@@ -26,30 +26,3 @@ const Research = () => {
 
 export default Research;
 
-// import React, { useState } from 'react';
-// import ResearchForm from './ResearchForm';
-// import ResearchResults from './ResearchResults';
-// import { useResearch } from '../../hooks/useResearch';
-
-// const Research = () => {
-//   const [query, setQuery] = useState('');
-//   const { loading, results, performResearch } = useResearch();
-
-//   const handleResearch = async (searchQuery) => {
-//     await performResearch(searchQuery);
-//   };
-
-//   return (
-//     <div className="space-y-4 sm:space-y-6 pb-4 sm:pb-0">
-//       <ResearchForm 
-//         query={query}
-//         setQuery={setQuery}
-//         onSubmit={handleResearch}
-//         loading={loading}
-//       />
-//       <ResearchResults results={results} />
-//     </div>
-//   );
-// };
-
-// export default Research;
